f74339d4c6612f96562e4df568abf30d72a81c4a
Andy Walpole <me@andywalpole.me>
2018-05-26 19:58:30 +0100

    nrew redux todo example

--- app/redux-example.js
+++ app/redux-example.js
@@ -1,8 +1,26 @@
 import { createStore } from 'redux';
 
-const reducer = (state = { name: 'Anonymous' }, action) => {
-	return state;
+const reducer = (state = {name: 'Anonymous'}, action) => {
+	console.log('new action');
+	switch (action.type) {
+		case 'CHANGE_NAME':
+			return {
+				...state,
+				name: action.name
+			};
+		default:
+			return state;
+	}
 };
 const store = createStore(reducer);
 const currentState = store.getState();
 console.log('currentState', currentState);
+
+const action = {
+	type: 'CHNAGE_NAME',
+	name: 'Andrew'
+};
+
+store.dispatch(action);
+
+console.log('Name should be andrew', store.getState());
--- app/redux-todo-example.js
+++ app/redux-todo-example.js
@@ -1,14 +1,38 @@
 import { createStore } from 'redux';
 
+const stateDefault = {
+	todos: [],
+	searchText: '',
+	showCompleted: false,
+}
+
 const reducer = (
-	state = {
-		todos: [],
-		searchText: '',
-		showCompleted: false,
-	},
-	action) => {
-	return state;
+	state = stateDefault,
+	action,
+) => {
+
+	switch (action.type) {
+		case 'CHANGE_SEARCH_TEXT':
+			return {
+				...state,
+				searchText: action.searchText
+			}
+		default:
+			return state;
+	}
+
 };
+
 const store = createStore(reducer);
 const currentState = store.getState();
+
 console.log('currentState', currentState);
+
+store.dispatch({
+	type: 'CHANGE_SEARCH_TEXT',
+	searchText: 'work',
+});
+
+console.log('searchText should be '\'work\'', currentState);
+
+
