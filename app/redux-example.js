ddae9e5f20493cec2317dd6e2c0d555fa37f9536
Andy Walpole <me@andywalpole.me>
2018-05-26 12:03:03 +0100

    added redux example

--- app/redux-example.js
+++ app/redux-example.js
@@ -1,1 +1,8 @@
-import Redux from 'redux';
+import { createStore } from 'redux'
+
+const reducer = (state = { name: 'Anonymous' }, action) => {
+	return state;
+};
+const store = createStore(reducer);
+const currentState = store.getState();
+console.log('currentState', currentState);
