8cb1a7316ee0629d00c2eeedfc124545f9cded9b
Andy Walpole <me@andywalpole.me>
2018-05-27 06:47:21 +0100

    another example to addt to reducer SWITCH statement

--- app/redux-example.js
+++ app/redux-example.js
@@ -1,17 +1,49 @@
 import { createStore, compose } from 'redux';
 
-const reducer = (state = {name: 'Anonymous'}, action) => {
-	console.log('new action');
+const stateDefault = {
+	name: 'Anonymous',
+	hobbies: [],
+	movies: [],
+};
+let nextHobbyId = 1;
+let nextMovieId = 1;
+const reducer = (state = stateDefault, action) => {
+
 	switch (action.type) {
 		case 'CHANGE_NAME':
 			return {
 				...state,
 				name: action.name
+			}
+		case 'ADD_HOBBY':
+			return {
+				...state,
+				hobbies: [
+					...state.hobbies,
+					{
+						id: nextHobbyId =+ 1,
+						hobby: action.name
+					}
+				]
+			};
+		case 'ADD_MOVIE':
+			return {
+				...state,
+				movies: [
+					...state.movies,
+					{
+						id: nextMovieId += 1,
+						title: action.title,
+						genre: action.genre
+					}
+				]
 			};
 		default:
 			return state;
 	}
+
 };
+
 const store = createStore(reducer, compose(
 	window.devToolsExtension ? window.devToolsExtension() : f => f,
 ));
@@ -19,31 +51,36 @@ const store = createStore(reducer, compose(
 // Subscribe to changes
 const unsubscribe = store.subscribe(() => {
 	const state = store.getState();
+	console.dir(state);
 
-	document.getElementById('root').innerHTML = null;
+	document.getElementById('root').innerHTML = state.name;
 	console.log('name is', state.name);
 });
 
 const currentState = store.getState();
 console.log('currentState', currentState);
 
-const action = {
-	type: 'CHNAGE_NAME',
-	name: 'Andrew'
-};
-
-store.dispatch(action);
-
 store.dispatch({
 	type: 'CHANGE_NAME',
 	name: 'Andrew'
 });
 
-unsubscribe();
+store.dispatch({
+	type: 'ADD_HOBBY',
+	name: 'Running'
+});
+
+// unsubscribe();
 
 store.dispatch({
 	type: 'CHANGE_NAME',
 	name: 'Jim'
+});
+
+store.dispatch({
+	type: 'ADD_MOVIE',
+	title: 'Die Hard',
+	genre: 'Action film'
 });
 
 
